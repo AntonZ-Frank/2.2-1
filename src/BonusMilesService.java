1f801392210d67d28d9eb51c9af37ce104246337
AntonZ-Frank <toni.rus@yandex.ru>
2020-03-05 12:42:29 +0500

    форматирование кода

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,9 +1,7 @@
 public class BonusMilesService {
-   public int calculate (int price)
-   {
-int cost = 20; // цена бонуса
-      int miles = price / cost;
-return miles;
-
-   }
+    public int calculate(int price) {
+        int cost = 20; // цена бонуса
+        int miles = price / cost;
+        return miles;
+    }
 }
